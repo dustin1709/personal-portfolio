2b79169904affb929a62f8ad51202f0656834b83
dustin1709 <duong.chu.vu@lalumiere.org>
2022-06-20 14:30:14 +0700

    m

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 import './App.css';
 import Overview from './Pages/Overview/Overview';
 import Experience from "./Pages/Experience/Experience";
+import Projects from "./Pages/Projects/Projects";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Overview />} />
           <Route path="/experience" element={<Experience />} />
+          <Route path="/projects" element={<Projects />} />
         </Routes>
       </main>
     </div>
---
+++ src/Pages/Projects/Projects.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "../../Components/Footer/Footer";
+import NavBar from "../../Components/NavBar/NavBar";
+
+const Projects = () => {
+    return (
+        <>
+            <NavBar />
+            <Footer pageId={"second"}/>
+        </>
+    );
+}
+
+export default Projects
